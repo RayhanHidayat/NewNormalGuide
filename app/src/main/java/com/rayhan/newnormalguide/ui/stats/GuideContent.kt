3d3d0a856f723351c15b7c26f19ab01a8d24e984
rayhan_hidayat <rayhananh2001@gmail.com>
2021-07-09 23:20:04 +0700

    add data for covid recycler view

--- app/src/main/java/com/rayhan/newnormalguide/ui/stats/GuideContent.kt
+++ app/src/main/java/com/rayhan/newnormalguide/ui/stats/GuideContent.kt
@@ -1,5 +1,6 @@
 package com.rayhan.newnormalguide.ui.stats
 
+import com.rayhan.newnormalguide.R
 import com.rayhan.newnormalguide.ui.guide.GuideData
 
 object GuideContent {
@@ -10,13 +11,50 @@ object GuideContent {
             GuideData(
                 null,
                 "Mengenal Covid-19",
-                null,
-                "alodokter",
-                "..",
-                "..",
-                ".."
+                R.drawable.ic_baseline_verified_user_24,
+                "alodokter.com",
+                "dr. Merry Dame Cristy Pane",
+                "Virus Corona atau severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2) adalah virus yang menyerang sistem pernapasan. Penyakit karena infeksi virus ini disebut COVID-19. Virus Corona bisa menyebabkan gangguan ringan pada sistem pernapasan, infeksi paru-paru yang berat, hingga kematian.",
+                "Severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2) yang lebih dikenal dengan nama virus Corona adalah jenis baru dari coronavirus yang menular ke manusia. Virus ini bisa menyerang siapa saja, seperti lansia (golongan usia lanjut), orang dewasa, anak-anak, dan bayi, termasuk ibu hamil dan ibu menyusui.\nInfeksi virus Corona disebut COVID-19 (Corona Virus Disease 2019) dan pertama kali ditemukan di kota Wuhan, China pada akhir Desember 2019. Virus ini menular dengan sangat cepat dan telah menyebar ke hampir semua negara, termasuk Indonesia, hanya dalam waktu beberapa bulan.\nHal tersebut membuat beberapa negara menerapkan kebijakan untuk memberlakukan lockdown dalam rangka mencegah penyebaran virus Corona. Di Indonesia sendiri, diberlakukan kebijakan Pembatasan Sosial Berskala Besar (PSBB) untuk menekan penyebaran virus ini.\nCoronavirus adalah kumpulan virus yang bisa menginfeksi sistem pernapasan. Pada banyak kasus, virus ini hanya menyebabkan infeksi pernapasan ringan, seperti flu. Namun, virus ini juga bisa menyebabkan infeksi pernapasan berat, seperti infeksi paru-paru (pneumonia)."
             )
         )
+
+        data.add(
+            GuideData(
+                null,
+                "Mencegah Covid-19",
+                R.drawable.ic_baseline_verified_user_24,
+                "alodokter.com",
+                "dr. Merry Dame Cristy Pane",
+                "Saat ini, Indonesia sedang melakukan vaksinasi COVID-19 secara berkala ke masyarakat Indonesia. Meskipun vaksinasi sudah mulai di jalankan, cara pencegahan yang terbaik adalah dengan menghindari faktor-faktor yang bisa menyebabkan Anda terinfeksi virus ini, yaitu:",
+                "1. Terapkan physical distancing, yaitu menjaga jarak minimal 1 meter dari orang lain, dan jangan dulu ke luar rumah kecuali ada keperluan mendesak.\n2. Gunakan masker saat beraktivitas di tempat umum atau keramaian, termasuk saat pergi berbelanja bahan makanan dan mengikuti ibadah di hari raya, misalnya Idul Adha.\n3. Rutin mencuci tangan dengan air dan sabun atau hand sanitizer yang mengandung alkohol minimal 60%, terutama setelah beraktivitas di luar rumah atau di tempat umum.\n4. Jangan menyentuh mata, mulut, dan hidung sebelum mencuci tangan.\n5. Meningkatkan daya tahan tubuh dengan pola hidup sehat, seperti mengonsumsi makanan bergizi, berolahraga secara rutin, beristirahat yang cukup, dan mencegah stres.\n6. Hindari kontak dengan penderita COVID-19, orang yang dicurigai positif terinfeksi virus Corona, atau orang yang sedang sakit demam, batuk, atau pilek.\n7. Tutup mulut dan hidung dengan tisu saat batuk atau bersin, kemudian buang tisu ke tempat sampah\n8. Jaga kebersihan benda yang sering disentuh dan kebersihan lingkungan, termasuk kebersihan rumah."
+            )
+        )
+
+        data.add(
+            GuideData(
+                null,
+                "Menginndikasi Covid-19",
+                R.drawable.ic_baseline_verified_user_24,
+                "alodokter.com",
+                "dr. Merry Dame Cristy Pane",
+                "Untuk menentukan apakah pasien terinfeksi virus Corona, dokter akan menanyakan gejala yang dialami pasien dan apakah pasien baru saja bepergian atau tinggal di daerah yang memiliki kasus infeksi virus Corona sebelum gejala muncul. Dokter juga akan menanyakan apakah pasien ada kontak dengan orang yang menderita atau diduga menderita COVID-19.",
+                "Guna memastikan diagnosis COVID-19, dokter akan melakukan beberapa pemeriksaan berikut:\n1. Rapid test untuk mendeteksi antibodi (IgM dan IgG) yang diproduksi oleh tubuh untuk melawan virus Corona. Namun, rapid test antigen sekarang dianggap lebih akurat dibandingkan rapid antibodi. Meski demikian, pemeriksaan rapid test atau swab antigen tidak dianjurkan untuk dilakukan mandiri di rumah dan harus oleh petugas medis.\n2. Swab test atau tes PCR (polymerase chain reaction) untuk mendeteksi virus Corona di dalam dahak.\n3. CT scan atau Rontgen dada untuk mendeteksi infiltrat atau cairan di paru-paru\nSelain itu, tes menggunakan alat GeNose juga bisa digunakan sebagai skrining atau pemeriksaan awal untuk mendeteksi virus Corona.\nHasil rapid test COVID-19 atau tes GeNose positif kemungkinan besar menunjukkan bahwa Anda memang sudah terinfeksi virus Corona, namun bisa juga berarti Anda terinfeksi kuman atau virus yang lain. Sebaliknya, hasil rapid test COVID-19 negatif belum tentu menandakan bahwa Anda mutlak terbebas dari virus Corona."
+            )
+        )
+
+        data.add(
+            GuideData(
+                null,
+                "Mengobati Covid-19",
+                R.drawable.ic_baseline_verified_user_24,
+                "alodokter.com",
+                "dr. Merry Dame Cristy Pane",
+                "Belum ada obat yang benar-benar efektif untuk mengatasi infeksi virus Corona atau COVID-19. Pilihan pengobatan akan disesuaikan dengan kondisi pasien dan tingkat keparahannya. ",
+                "Beberapa pasien dengan gejala ringan atau tanpa gejala akan di sarankan untuk melakukan protokol isolasi mandiri di rumah sambil tetap melakukan langkah pencegahan penyebaran infeksi virus Corona.\nSelain itu, dokter juga bisa memberikan beberapa beberapa langkah untuk meredakan gejalanya dan mencegah penyebaran virus corona, yaitu:\n1. Merujuk penderita COVID-19 yang berat untuk menjalani perawatan dan karatina di rumah sakit rujukan\n2. Memberikan obat pereda demam dan nyeri yang aman dan sesuai kondisi penderita\n3. Menganjurkan penderita COVID-19 untuk melakukan isolasi mandiri dan istirahat yang cukup\n4. Menganjurkan penderita COVID-19 untuk banyak minum air putih untuk menjaga kadar cairan tubuh"
+            )
+        )
+
         return data
     }
 }
