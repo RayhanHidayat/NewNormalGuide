70ec16f550acd8ab650f2ffa54bc52f452bd824a
rayhan_hidayat <rayhananh2001@gmail.com>
2021-07-13 13:20:52 +0700

    progress on det news

---
+++ app/src/main/java/com/rayhan/newnormalguide/ui/detail_news/DetailNewsActivity.kt
@@ -0,0 +1,25 @@
+package com.rayhan.newnormalguide.ui.detail_news
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rayhan.newnormalguide.databinding.ActivityDetailNewsBinding
+
+class DetailNewsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailNewsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+}
